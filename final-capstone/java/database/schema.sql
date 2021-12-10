9d7e0b98bbf7d0229d124b4f2e4918d03183acf2
Annie Cochran <cochran.annie@gmail.com>
2021-12-10 12:30:23 -0500

    Database Update

--- final-capstone/java/database/schema.sql
+++ final-capstone/java/database/schema.sql
@@ -3,9 +3,11 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS family;
 DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS books;
 DROP SEQUENCE IF EXISTS seq_user_id;
 DROP SEQUENCE IF EXISTS seq_family_id;
 DROP SEQUENCE IF EXISTS seq_member_id;
+DROP SEQUENCE IF EXISTS seq_book_id;
 
 
 CREATE SEQUENCE seq_user_id
@@ -24,6 +26,13 @@ CREATE SEQUENCE seq_family_id
 CREATE SEQUENCE seq_member_id
   INCREMENT BY 1
   START WITH 101
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_book_id
+  INCREMENT BY 1
+  START WITH 1001
   NO MAXVALUE
   NO MINVALUE
   CACHE 1;
@@ -54,6 +63,15 @@ CREATE TABLE members (
     CONSTRAINT FK_members_family FOREIGN KEY (family_id) REFERENCES family(family_id)
 );
 
+CREATE TABLE books (
+    book_id int DEFAULT nextval ('seq_book_id'::regclass) NOT NULL,
+    book_name varchar(100) NOT NULL,
+    author varchar(100) NOT NULL,
+    family_id int,
+    CONSTRAINT PK_books PRIMARY KEY (book_id),
+    CONSTRAINT FK_books_family FOREIGN KEY (family_id) REFERENCES family(family_id)
+);
+
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 INSERT INTO users (username,password_hash,role) VALUES ('gilmore','password','ROLE_USER');
@@ -64,10 +82,17 @@ VALUES (501, 'banksfamily'),(502,'gilmore'), (503,'admin');
 
 INSERT INTO members (member_id, user_id, family_id, first_name, last_name, member_type)
 VALUES (101, 4, 501, 'Phil', 'Banks', 'Parent'),
-(102, null, 501, 'Vivanne', 'Banks', 'Parent'),
-(103, null, 501, 'Carlton', 'Banks', 'Child'),
-(104, 3, 502, 'Lorelai', 'Gilmore', 'Parent'),
-(105, 3, 502, 'Rory', 'Gilmore', 'Child');
+       (102, null, 501, 'Vivanne', 'Banks', 'Parent'),
+       (103, null, 501, 'Carlton', 'Banks', 'Child'),
+       (104, 3, 502, 'Lorelai', 'Gilmore', 'Parent'),
+       (105, 3, 502, 'Rory', 'Gilmore', 'Child');
+
+INSERT INTO books (book_id, book_name, author, family_id)
+VALUES (1001, 'Will', 'Will Smith', 501),
+       (1002, 'The Splendid and The Vile', 'Erik Larson', 501),
+       (1003, 'The Amber Spyglass', 'Philip Pullman', 501),
+       (1004, 'Nature Anatomy', 'Julia Rothman', 502),
+       (1005, 'The Innovators', 'Walter Isaacson', 502);
 
 
 COMMIT TRANSACTION;
